5e9f6cb38ac7f99b1f8831c3cf24e02b9ac9a43a
Liz <epringi@openconcept.ca>
2021-06-30 12:19:41 -0400

    Adding comments and organising code.

--- pytar.py
+++ pytar.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python3
+'''
+
+  PYTAR (aka: PYthon-guiTAR)
+------------------------------
+  Creds:
+    Liz Pringi <epringi@gmail.com>
+    Mike Mallett <mike@nerdcore.net>
+
+  This script takes the Guitar Hero guitar and makes it into a multi-purpose
+  keytar instrument.  This could potentially be compatible with other 'guitar'
+  devices, but current compatibility is only verified for RedOctane Guitar Hero X-plorer.
+
+---  Detected inputs:
+guitar.get_axis(x)
+  axis 3: whammy bar
+  axis 2: twist excellerometer
+  axis 4: up-down excellerometer
+  axis 5: ??
+
+guitar.get_button(x)
+  button 0: green
+  button 1: red
+  button 2: blue
+  button 3: yellow
+  button 4: orange
+  button 6: back
+  button 7: start
+  button 8: x-button
+
+guitar.get_hat(x)
+  hat 0: numpad + strum (strum only uses value[1])
+
+
+'''
 
 import pygame
 import signal
+
+
+#-- INIT
 
 # Exit gracefully if SIGINT
 def signal_handler(sig, frame):
@@ -9,7 +46,30 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# init joystick for guitar input and pygame for event handling of said guitar
+pygame.init()
+pygame.joystick.init()
+pygame.mixer.init()
 
+# Get all the joysticks detected
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+# We only want the guitar hero joystick (for now)
+for joy in joysticks:
+  if joy.get_name().find("Guitar")>=0:
+    guitar=joy
+
+# if we didn't find a guitar, then GTFO
+if 'guitar' not in locals():
+  print("No Guitar Hero device found")
+  exit()
+else:
+  print("Found a Guitar Hero device: ", guitar.get_name(), " - ", guitar.get_guid())
+
+
+#-- GLOBALS
+
+# For some nice output
 class colours:
   red    = '\033[91m'
   blue   = '\033[94m'
@@ -18,26 +78,6 @@ class colours:
   orange = '\033[95m'
   white  = '\033[1m'
   off    = '\033[0m'
-
-
-# init joystick for guitar input and pygame for event handling of said guitar
-pygame.init()
-pygame.joystick.init()
-pygame.mixer.init()
-
-'''thick=mixer.Sound(file)
-def fo(s, t):
-  s.play()
-  s.fadeout(t)
-
-fo(thick, 250)
-fo(thick, 750)
-fo(thick) 2000)
-
-mixer.music.load(file)
-mixer.music.play(4)
-mixer.music.queue(file)
-mixer.music.play(4)'''
 
 # some human readable values
 WHAMMY = 3
@@ -97,50 +137,14 @@ sounds = {
   'sampler': []
 }
 
-# Get all the joysticks detected
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-
-# we only want the guitar hero joystick
-for joy in joysticks:
-  if joy.get_name().find("Guitar")>=0:
-    guitar=joy
-
-# if we didn't find a guitar, then GTFO
-if 'guitar' not in locals():
-  print("No Guitar Hero device found")
-  exit()
-else:
-  print("Found a Guitar Hero device: ", guitar.get_name(), " - ", guitar.get_guid())
-
-'''  ---  Inputs and Locations
-guitar.get_axis(x)
-  3: whammy bar
-  2: twist excellerometer
-  4: up-down excellerometer
-  5: ??
-
-guitar.get_button(x)
-  0: green
-  1: red
-  2: blue
-  3: yellow
-  4: orange
-  6: back
-  7: start
-  8: x-button
-
-guitar.get_hat(x)
-  0: numpad + strum (strum only uses value[1])
-'''
-
 # Mode "guitar" or "sampler"
 mode = "guitar"
 
 # toggle value changed with back and start buttons
 toggle = 0
 
-# State and sound info for the inputs
-inputs = {
+# State and sound info for the buttons
+buttons = {
   'green': {
     'state': False,
     #'value': colours.green + "green" + colours.off
@@ -171,16 +175,15 @@ inputs = {
     #'sound': { 1: sounds['bnote'], -1: sounds['gnote'] }
     'sound': {}
   }
-  #'whammy': {
-  #  'state': False,
-  #  'value': colours.white + "Whammy" + colours.off
-  #}
 }
+
+
+#-- MEAT
 
 # set the sounds for the buttons
 def set_sounds():
   if mode == "guitar":
-    for button, values in inputs.items():
+    for button, values in buttons.items():
       values['sound'][UP]=sounds['guitar'][toggle][UP][button]
       values['sound'][DOWN]=sounds['guitar'][toggle][DOWN][button]
 
@@ -192,39 +195,39 @@ while True:
 
     # Detect depressed buttons :'(
     if e.type == pygame.JOYBUTTONUP and e.button == GREEN:
-      inputs['green']['state'] = False
+      buttons['green']['state'] = False
     if e.type == pygame.JOYBUTTONDOWN and e.button == GREEN:
-      inputs['green']['state'] = True
+      buttons['green']['state'] = True
       #print(colours.green + "green note active" + colours.off)
 
     if e.type == pygame.JOYBUTTONUP and e.button == RED:
-      inputs['red']['state'] = False
+      buttons['red']['state'] = False
     if e.type == pygame.JOYBUTTONDOWN and e.button == RED:
-      inputs['red']['state'] = True
+      buttons['red']['state'] = True
       #print(colours.red + "red note active" + colours.off)
 
     if e.type == pygame.JOYBUTTONUP and e.button == YELLOW:
-      inputs['yellow']['state'] = False
+      buttons['yellow']['state'] = False
     if e.type == pygame.JOYBUTTONDOWN and e.button == YELLOW:
-      inputs['yellow']['state'] = True
+      buttons['yellow']['state'] = True
       #print(colours.yellow + "yellow note active" + colours.off)
 
     if e.type == pygame.JOYBUTTONUP and e.button == BLUE:
-      inputs['blue']['state'] = False
+      buttons['blue']['state'] = False
     if e.type == pygame.JOYBUTTONDOWN and e.button == BLUE:
-      inputs['blue']['state'] = True
+      buttons['blue']['state'] = True
       #print(colours.blue + "blue note active" + colours.off)
 
     if e.type == pygame.JOYBUTTONUP and e.button == ORANGE:
-      inputs['orange']['state'] = False
+      buttons['orange']['state'] = False
     if e.type == pygame.JOYBUTTONDOWN and e.button == ORANGE:
-      inputs['orange']['state'] = True
+      buttons['orange']['state'] = True
       #print(colours.orange + "orange note active" + colours.off)
 
     '''if e.type == pygame.JOYAXISMOTION and e.axis == 3 and e.value < -1.000030518509476:
-      inputs['whammy']['state'] = False
+      buttons['whammy']['state'] = False
     if e.type == pygame.JOYAXISMOTION and e.axis == 3 and e.value > -1.000030518509476:
-      inputs['whammy']['state'] = True
+      buttons['whammy']['state'] = True
       #print(colours.white + "Whammy - alter noise" + colours.off + str(e.value))'''
 
     # toggle the toggle if the toggle is toggled :|
@@ -233,13 +236,13 @@ while True:
       set_sounds()
 
     # Strum action
-    if e.type == pygame.JOYHATMOTION and e.value[1] != 0:
-      # We use the strum value if in guitar mode, otherwise switch to the next sample set
-      strum_direction = UP
-      if mode == "guitar":
-        strum_direction = e.value[1]
+    strum_direction = UP
 
-      for input, attr in inputs.items():
+    if e.type == pygame.JOYHATMOTION and e.value[1] != 0 and mode == 'guitar':
+      strum_direction = e.value[1]
+
+      # play all the sounds
+      for input, attr in buttons.items():
         if attr['state'] == True:
           attr['sound'][UP].stop()
           attr['sound'][DOWN].stop()
@@ -252,5 +255,3 @@ while True:
 
     if e.type == pygame.JOYBUTTONDOWN and e.button == XBUTTON:
       exit()
-    #if e.type == pygame.JOYAXISMOTION and e.axis == 4:
-      #print("drama")
